405440ff7bdae059729ee589d51a1f9de1c1a473
Jenniscott <scottbellojennifer@gmail.com>
2024-03-12 18:35:44 -0700

    corrections

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for k in range(i + 1, 10):
-        print("{:d}{:d}".format(k, j), end=', ')
-print("{:d}{:d}".format(k + 1, j))
+for i in range(0, 10):
+    for k in range((i + 1), 10):
+        if (i != 8) or (k != 9):
+            print("{}{}, ".format(i, k), end="")
+        else:
+            print("{]{}".format(i, k))
