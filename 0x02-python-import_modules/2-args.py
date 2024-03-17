c76f2beb27e16d8b7c16bf514830cefeb57acdd8
Jenniscott <scottbellojennifer@gmail.com>
2024-03-17 14:55:53 -0700

    python scripts

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pythion3
+if __name__ = "__main__":
+        import sys
+
+        if arg == 0:
+                print("{} arguments".format(arg))
+            elif arg == 1:
+                print("{} argument".format(arg))
+            else:
+                print("{} arguments: ".format(arg))
+
+            for j in range(arg):
+                print("{} : {}".format(j+1, sys.arg(i+1))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    from sys import argv
+
+        argc = len(argv)
+            addition = 0
+
+            for j in range(1, argc):
+                    result += int(argv[j])
+                        print("{}".format(addition)))
